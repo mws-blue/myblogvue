e9118c4400f3e2b08bb76ca51726d2bb7eae1da7
2471377122 <2471377122@qq.com>
2020-03-03 14:37:36 +0800

    添加了about路由，端口号统一使用8888

--- vuecli-front/src/views/About.vue
+++ vuecli-front/src/views/About.vue
@@ -1,5 +1,24 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>{{Hello}}</h1>
   </div>
 </template>
+<script>
+import axios from "axios";
+export default {
+    data:function(){
+        return {
+        Hello:""
+        }
+    },
+    created(){
+        axios
+        .get("http://localhost:8888/about", {
+        })
+        .then(res => {
+            console.log(res.data)
+          this.Hello = res.data;
+        });
+    }
+}
+</script>
