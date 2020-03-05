879fc0a901732ab5f55dc1b980fe5a49e1b838eb
2471377122 <2471377122@qq.com>
2020-03-05 13:10:21 +0800

    增添控制器文件login文件

---
+++ webpack-vue-server/app/controller/login.js
@@ -0,0 +1,33 @@
+/* eslint-disable eqeqeq */
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-trailing-spaces */
+/* eslint-disable spaced-comment */
+/* eslint-disable no-undef */
+/* eslint-disable prefer-const */
+/* eslint-disable eol-last */
+/* eslint-disable indent */
+'use strict';
+
+const Controller = require('egg').Controller;
+
+class LoginController extends Controller {
+    async index() {
+        const { ctx, service } = this;
+        let formData = ctx.request.query;
+        let result = await service.login.login(formData.username);
+        let data = { code: 1, Msg: '登录成功' };
+        if (!result) {
+            data = { code: -1, Msg: '账号不存在' };
+        }
+        //密码错误 
+        if (result && formData.password != result.password) {
+
+            data = { code: 0, Msg: '密码错误' };
+        }
+        ctx.body = data;
+        console.log(formData);
+        console.log(result); //:数据库查询来的数据
+    }
+}
+
+module.exports = LoginController;
