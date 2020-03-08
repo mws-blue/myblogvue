81aa92be940c36b1796063e55f0a90147a112cf7
2471377122 <2471377122@qq.com>
2020-03-08 08:36:37 +0800

    添加插件

--- webpack-vue-server/config/plugin.js
+++ webpack-vue-server/config/plugin.js
@@ -1,21 +1,24 @@
+/* eslint-disable eol-last */
+/* eslint-disable indent */
+/* eslint-disable comma-dangle */
+/* eslint-disable spaced-comment */
 'use strict';
 
 /** @type Egg.EggPlugin */
 module.exports = {
-  // had enabled by egg
-  // static: {
-  //   enable: true,
-  // }
+    // had enabled by egg
+    // static: {
+    //   enable: true,
+    // }
 
-  //开启mysql
-  mysql: {
-    enable: true,
-    package: 'egg-mysql',
-  },
-  //开启cors
-  cors: {
-    enable: true,
-    package: 'egg-cors',
-  }
+    //开启mysql
+    mysql: {
+        enable: true,
+        package: 'egg-mysql',
+    },
+    //开启cors
+    cors: {
+        enable: true,
+        package: 'egg-cors',
+    }
 };
-
