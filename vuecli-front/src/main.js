5b3e0cd1e1ec5ceb94d3c8638e9ebed2e7fbda05
2471377122 <2471377122@qq.com>
2020-03-08 08:24:10 +0800

    添加了axios请求基址，但是有一些地方没有用

--- vuecli-front/src/main.js
+++ vuecli-front/src/main.js
@@ -3,14 +3,17 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
+import axios from 'axios'
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 
 Vue.use(ElementUI);
 Vue.config.productionTip = false;
 
+Vue.prototype.$http = axios
+
 new Vue({
-  router,
-  store,
-  render: h => h(App)
+    router,
+    store,
+    render: h => h(App)
 }).$mount('#app')
