401a2ebe75abf2cbb4be9967bf873c0653ea2347
2471377122 <2471377122@qq.com>
2020-03-08 08:32:09 +0800

    添加路由接口

--- webpack-vue-server/app/router.js
+++ webpack-vue-server/app/router.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-trailing-spaces */
+/* eslint-disable spaced-comment */
 /* eslint-disable indent */
 /* eslint-disable eol-last */
 'use strict';
@@ -8,7 +10,11 @@
 module.exports = app => {
     const { router, controller } = app;
     router.get('/', controller.home.index);
-    router.get('/Login', controller.login.index);
-    router.post('/register', controller.register.index);
-
+    router.get('/Login', controller.login.index); //用户登录
+    router.post('/register', controller.register.index); //用户注册
+    router.post('/upload/', controller.upload.index); //上传头像
+    router.get('/user', controller.user.index); //获取用户所有信息
+    router.post('/changeinfo', controller.changeinfo.index); //修改用户基本所有信息
+    router.get('/getarticle', controller.getarticle.index); //获取用户文章所有信息
+    router.post('/delarticle', controller.delarticle.index); //删除用户文章所有信息-
 };
